09a392dc36bdeca95fae34393be9816618fcf9c4
Joseph Orawiec <143203535+Joseph-Orawiec@users.noreply.github.com>
2026-06-20 16:59:47 -0500

    Placeholder Asset Code

--- global.gd
+++ global.gd
@@ -1,1 +1,27 @@
 extends Node
+
+
+const sound_dictionary = {
+	"BGM": {
+		"reeling_em_in": preload("res://icon.svg"),
+		"pier": preload("res://icon.svg"),
+		"stadium": preload("res://icon.svg"),
+		"whirlpool": preload("res://icon.svg"),
+	},
+	"SFX": {
+		"speak": {
+			"player": preload("res://icon.svg"),
+			"business_fish": preload("res://icon.svg"),
+			"business_wife_fish": preload("res://icon.svg"),
+			"futbol_fish": preload("res://icon.svg"),
+		},
+		
+		"salmon_peeling_dying": preload("res://icon.svg"),
+		"salmon_glorious": preload("res://icon.svg"),
+		"goalie_noises": preload("res://icon.svg"),
+		
+		"rod_reeling": preload("res://icon.svg"),
+		"caught_fish": preload("res://icon.svg")
+		
+	}
+}
