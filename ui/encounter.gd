8e12cfee2013589aa230c546764c2ee7f4bccd23
Julian Laxamana <julianlaxamana123@gmail.com>
2026-06-19 23:03:23 -0700

    Dialogue engine working

--- ui/encounter.gd
+++ ui/encounter.gd
@@ -1,17 +1,40 @@
 extends Control
 
 var a = 0
+var index = 0
+var names
+var dialogue
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	dialogue = load_dialogue("res://assets/dialogue/test.json")["test"]
+	names = dialogue["position"]
+	$leftSprite.texture = load(dialogue[dialogue["position"][0]])
+	$rightSprite.texture = load(dialogue[dialogue["position"][1]])
+	$NinePatchRect/RichTextLabel.text = dialogue["dialogue"][index]["text"]
+	$NinePatchRect2/RichTextLabel.text = dialogue["dialogue"][index]["speaker"]
 	$NinePatchRect/RichTextLabel.visible_characters = 0
 	pass # Replace with function body.
-
+	
+func load_dialogue(file_path: String) -> Dictionary:
+	if FileAccess.file_exists(file_path):
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		var json_text = file.get_as_text()
+		var json = JSON.new()
+		if json.parse(json_text) == OK:
+			return json.data
+	return {}
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("click") and $GrassButtonsArrow.visible:
-		print("next")
+		index += 1
+		$NinePatchRect/RichTextLabel.visible_characters = 0
+		$NinePatchRect/RichTextLabel.text = dialogue["dialogue"][index]["text"]
+		$NinePatchRect2/RichTextLabel.text = dialogue["dialogue"][index]["speaker"]
 		$GrassButtonsArrow.visible = false
+		
 	a += delta
 	$GrassButtonsArrow.position.x = 1221.0 + 2.5 * sin(a * 5)
 	pass
@@ -19,6 +42,16 @@ func _process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	if $NinePatchRect/RichTextLabel.visible_characters < len($NinePatchRect/RichTextLabel.text):
+		if names.find(dialogue["dialogue"][index]["speaker"]) == 1:
+			$rightSprite.modulate = Color(1.0, 1.0, 1.0, 1.0)
+			$rightSprite.scale = lerp($rightSprite.scale, Vector2(0.9, -0.9), 0.25)
+			$leftSprite.modulate = Color(0.5, 0.5, 0.5, 1.0)
+			$leftSprite.scale = lerp($leftSprite.scale, Vector2(0.7, 0.7), 0.25)
+		else:
+			$leftSprite.modulate = Color(1.0, 1.0, 1.0, 1.0)
+			$rightSprite.scale = lerp($rightSprite.scale, Vector2(0.7, -0.7), 0.25)
+			$rightSprite.modulate = Color(0.5, 0.5, 0.5, 1.0)
+			$leftSprite.scale = lerp($leftSprite.scale, Vector2(0.9, 0.9), 0.25)
 		$NinePatchRect/RichTextLabel.visible_characters += 1
 		$GrassButtonsArrow.visible = false
 	else:
