1249789ec156a426379effeecf692bea1f075aff
Joseph Orawiec <143203535+Joseph-Orawiec@users.noreply.github.com>
2026-06-22 19:59:15 -0500

    UIDs are cool

--- global.gd
+++ global.gd
@@ -3,27 +3,27 @@ extends Node
 
 const sound_dictionary = {
 	"BGM": {
-		"reeling_em_in": preload("res://icon.svg"),
-		"pier": preload("res://icon.svg"),
-		"stadium": preload("res://icon.svg"),
-		"whirlpool": preload("res://icon.svg"),
+		"reeling_em_in": preload("uid://domx4xdpapir"),
+		"pier": preload("uid://domx4xdpapir"),
+		"stadium": preload("uid://domx4xdpapir"),
+		"whirlpool": preload("uid://domx4xdpapir"),
 	},
 	
 	"SFX": {
 		
 		"speak": {
-			"player": preload("res://icon.svg"),
-			"business_fish": preload("res://icon.svg"),
-			"business_wife_fish": preload("res://icon.svg"),
-			"futbol_fish": preload("res://icon.svg"),
+			"player": preload("uid://domx4xdpapir"),
+			"business_fish": preload("uid://domx4xdpapir"),
+			"business_wife_fish": preload("uid://domx4xdpapir"),
+			"futbol_fish": preload("uid://domx4xdpapir"),
 		},
 		
-		"salmon_peeling_dying": preload("res://icon.svg"),
-		"salmon_glorious": preload("res://icon.svg"),
-		"goalie_noises": preload("res://icon.svg"),
+		"salmon_peeling_dying": preload("uid://domx4xdpapir"),
+		"salmon_glorious": preload("uid://domx4xdpapir"),
+		"goalie_noises": preload("uid://domx4xdpapir"),
 		
-		"rod_reeling": preload("res://icon.svg"),
-		"caught_fish": preload("res://icon.svg")
+		"rod_reeling": preload("uid://domx4xdpapir"),
+		"caught_fish": preload("uid://domx4xdpapir")
 		
 	}
 }
