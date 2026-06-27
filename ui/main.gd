a0198c75add7ef0c3475b5f535f217c1e1775a92
julianlaxamana <julianlaxamana123@gmail.com>
2026-06-26 22:55:24 -0700

    goon

--- ui/main.gd
+++ ui/main.gd
@@ -4,3 +4,20 @@ extends Control
 func _on_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://game/dock2.tscn")
 	pass # Replace with function body.
+
+
+func _on_button_2_button_up() -> void:
+	$Credits.visible = !$Credits.visible 
+	$CreditsBox.visible = !$CreditsBox.visible 
+	pass # Replace with function body.
+
+
+func _on_texture_button_button_up() -> void:
+	$Credits.visible = !$Credits.visible 
+	$CreditsBox.visible = !$CreditsBox.visible 
+	pass # Replace with function body.
+
+
+func _on_texture_button_2_button_up() -> void:
+	get_tree().change_scene_to_file("res://game/dock2.tscn")
+	pass # Replace with function body.
